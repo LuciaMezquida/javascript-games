23ad6cc0fb461d483d158b4193987b8e92fc62a1
Lucia <lucia.mezquida@gmail.com>
2020-05-25 19:36:31 +0200

    Collect the grid divs into an array

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div')); // To colect the 200 div and put them into an array
+    const scoreDisplay = document.querySelector('#score');
+    const startButton = document.querySelector('#start-button');
+    const width = 10;
+
+
+    console.log(squares)
 
     
 })
