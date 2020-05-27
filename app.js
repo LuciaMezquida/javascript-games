7b51197d815dd7c24128d04cf9d0a72ceb804c05
Lucia <lucia.mezquida@gmail.com>
2020-05-27 19:59:02 +0200

    Drawing tetrominoes and creating its rotations

--- app.js
+++ app.js
@@ -3,10 +3,67 @@ document.addEventListener('DOMContentLoaded', () => {
     let squares = Array.from(document.querySelectorAll('.grid div')); // To colect the 200 div and put them into an array
     const scoreDisplay = document.querySelector('#score');
     const startButton = document.querySelector('#start-button');
-    const width = 10;
+    const width = 10; // used to draw the tetrominos
 
+    //the tetrominos
+    //each of the four arrays is one of its rotations
+    const lTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ]
 
-    console.log(squares)
+    const zTetromino = [
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2,   width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2,   width * 2, width * 2 + 1]
+    ]
 
-    
+    const tTetromino = [
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1]
+    ]
+
+    const iTetromino = [
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3]
+    ]
+
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+    //We're going to start the tetrominoes rotation at the square with index 4
+    let currentPosition = 4;
+    let currentRotation = 0; //tetromino will start in its first rotation
+    //randomly select a tetromino and its first rotation
+    let random = Math.floor(Math.random() * theTetrominoes.length);
+    let current = theTetrominoes[random][currentRotation];
+    //let's write a function to draw the first rotation of the first random tetromino
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino');
+        })
+    }
+
+    //undraw the tetromino: unroll the randomly chosen tetromino and its current rotation
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+
+    draw();
+
 })
