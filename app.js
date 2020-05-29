1cfd4d3bac8befcd10c4a4f64e30802217bea674
Lucia <lucia.mezquida@gmail.com>
2020-05-29 18:36:23 +0200

    Established the keyCodes to move the tetrominoes and made their rotation

--- app.js
+++ app.js
@@ -66,6 +66,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //make the tetromino move down every second
     timerId = setInterval(moveDown, 1000);
+
+    //assign functions to keyCodes
+    function control(e) {
+        if (e.keyCode === 37) {
+            moveLeft();
+        } else if (e.keyCode === 32) {
+            rotate()
+        } else if (e.keyCode === 39) {
+            moveRight()
+        } else if (e.keyCode === 40) {
+            moveDown()
+        }
+    }
+    document.addEventListener('keyup', control);
+
     //move function
     function moveDown() {
         undraw();
@@ -94,6 +109,29 @@ document.addEventListener('DOMContentLoaded', () => {
         if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
             currentPosition += 1;
         }
+        draw();
+    }
+
+    //move the tetromino right, unless is at the edge or there is a blockage
+    function moveRight() {
+        undraw();
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1);
+
+        if (!isAtRightEdge) currentPosition += 1;
+        if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -= 1;
+        }
+        draw();
+    }
+
+    //rotate de tetromino. If the current rotation gets to 4, make it go back to 0
+    function rotate() {
+        undraw();
+        currentRotation++;
+        if (currentRotation === current.length) { 
+            currentRotation = 0;
+        }
+        current = theTetrominoes[random][currentRotation];
         draw();
     }
 
