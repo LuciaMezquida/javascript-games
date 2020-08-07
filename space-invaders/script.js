467ded113525fd0af59b334785413e1f245113f5
Lucia <lucia.mezquida@gmail.com>
2020-08-07 19:29:07 +0200

    create indexes for the invaders and add class to style them

--- space-invaders/script.js
+++ space-invaders/script.js
@@ -1,12 +1,23 @@
 document.addEventListener('DOMContentLoaded', () =>{
     //let's pick out the classes and ids with querySelectors
-    const square = document.querySelectorAll('.grid div');
+    const squares = document.querySelectorAll('.grid div');
     const resultDisplay = document.querySelector('#result');
     let width = 15; //we want the width of the grid to be 15
     let currentShooterIndex = 202; //we want the shooter to start at index 202
-    let currentInvederIndex = 0; //we want our invader to start at index 0
+    let currentInvaderIndex = 0; //we want our invader to start at index 0
     let alienInvadersTakenDown = []; //invaders we have shot down
     let result = 0;
     let direction = 1; //the direction we want to go
     let invaderId = null;
+
+    //Defining the alien invaders
+    const alienInvaders = [0 ,1, 2, 3, 4, 5, 6, 7, 8, 9,            //indexes we want our invaders to be in
+                           15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+                           30, 31, 32, 33, 34, 35, 36, 37, 38, 39];
+    
+    //Draw the alien invaders
+    alienInvaders.forEach( invader => squares[currentInvaderIndex + invader].classList.add('invader'))
+
+
+
 })
